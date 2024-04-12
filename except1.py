8ced8e74e4a25d6c49852928031c9fda23312e44
fmoorenoo <fermontemore@gmail.com>
2024-04-12 12:27:10 +0100

    Avanzado except1.py

--- except1.py
+++ except1.py
@@ -2,12 +2,25 @@ import random
 def intDiv(a:int, b:int) -> int:
     try:
         return a // b
-    except:
-        print("Realizamos tarea de control de cierre")
+    except TypeError:
+        print("TypeError - Realizamos tarea de control de cierre")
+    except ZeroDivisionError:
+        print("ZeroDivisionError - Realizamos tarea de control de cierre")
+    except Exception:
+        print("Exception - Realizamos tarea de control de cierre")
 
 
 if __name__ == "__main__":
-    for i in range(30):
-        a = random.randint(0, 9)
-        b = random.randint(0, 9)
-        print(f"{a} // {b} = {intDiv(a, b)}")
+    def main()->None:
+        for i in range(30):
+            a = random.randint(0, 9)
+            b = random.randint(0, 9)
+            print(f"{a} // {b} = {intDiv(a, b)}")
+        
+        intDiv(2, "3")
+        try:
+            intDiv()
+        except:
+            print("Nolo consiguió")
+
+    main()
