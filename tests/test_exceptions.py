cd093d6b03b2825bf6705b738e9ce80549c48b23
fmoorenoo <fermontemore@gmail.com>
2024-04-15 13:04:28 +0100

    Avanzado text exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,7 +1,27 @@
 import pytest
 
+def divide(a:int, b:int):
+    if (b == 0):
+        raise ValueError("Cannot divide by Zero")
+    return a / b
+
+
 def test_exception():
     a = 0
     b = 10
     with pytest.raises(ZeroDivisionError):
         b / a
+
+    
+def test_divide():
+    with pytest.raises(ValueError) as exception_info:
+        divide(10, 0)
+    assert str(exception_info.value).lower == "Cannot divide by Zero".lower
+
+def test_recursion_depth():
+    with pytest.raises(RuntimeError) as exception_info:
+        def f():
+            f()
+
+        f()
+    assert "maximum recursion" in str(exception_info.value)
