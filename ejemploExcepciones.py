9771f5127e63d4e5d6ac89291838bf62c47c1d72
fmoorenoo <fermontemore@gmail.com>
2024-04-17 09:40:33 +0100

    Avanzado ejemploExcepciones.py

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -1,12 +1,40 @@
 class EjemploExcepciones:
     # ZeroDivisionError
+    def dividirEntreCero(self, a:int, b:int) ->int:
+        if (b == 0):
+            raise ZeroDivisionError
+
+        return a // b
+
 
     # ValueError
+    def valorIncorrecto(self, valor):
+        try:
+            return numero
+        except ValueError:
+            raise ValueError
+            
 
     # FileNotFoundError
+    def archivoNoEncontrado(self):
+        try:
+            with open(archivo, 'r') as file:
+                contenido = file.read()
+            return contenido
+
+        except FileNotFoundError:
+            raise FileNotFoundError("No se pudo encontrar el archivo")
+
 
     # TypeError
+    def errorDeTipo(self):
+        try:
+            a * b
 
+        except TypeError:
+            raise TypeError("No se puede operar con el tipo de dato proporcionado")
+
+    
     # IndexError
 
     # KeyboardInterrupt
