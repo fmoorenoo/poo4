6a8a15ab7ce1c9f39a2be609c46fada8be0e9ca8
fmoorenoo <fermontemore@gmail.com>
2024-05-07 12:01:19 +0100

    Avanzado minVoc.py

--- reg/minVoc.py
+++ reg/minVoc.py
@@ -6,4 +6,12 @@ class MinVoc:
 
     @staticmethod
     def buscar(texto:str) -> list:
-        patron = ""
+        patron = "\\b[aeiou]\\w*"
+        resultado = re.findall(patron, texto)
+        return resultado
+
+if __name__ == '__main__':
+    text = '''El auténtico mojo se elabora en mortero o almirez, lo que permite que cada ingrediente conserve su personalidad, y con todos los ingredientes en crudo, lo que hace que éstos conserven todas sus vitaminas y resultando por ello muy sanos. Aunque siempre hay una excepción a la regla, en este caso es el mojo palmero que lleva el ajo frito.
+    No obstante, existe una manera de preparar el mojo rojo que es más fácil y, sobre todo, que requiere de cero esfuerzo físico. El único requisito es contar con una batidora de mano y seguir la receta que os damos a continuación. En cinco minutos tendréis la salsa lista para su consumo.'''
+    
+    print(MinVoc.buscar(text))
