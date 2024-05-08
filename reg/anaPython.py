d59b0442db82c2e744e1bfee67bb675a1a0fa3ae
fmoorenoo <fermontemore@gmail.com>
2024-05-08 10:01:29 +0100

    Acabado contador de funciones y añadida la librería rich

--- reg/anaPython.py
+++ reg/anaPython.py
@@ -1,4 +1,30 @@
+import re
+import rich
+from rich import console, table
+
+
+console = console.Console()
+table = table.Table(header_style="green")
+table.add_column("Funciones", style="dim", width=12)
+table.add_column("Variables", style="dim", width=12)
+
+
 class AnaPython:
     @staticmethod
     def countDef(codigo: str) -> int:
-        pass
+        patron = "def\\s+\\w+\\(.*\\)\\s*(->\\s*\\w+)?\\s*:"
+        resultado = re.findall(patron, codigo)
+        return len(resultado)
+
+if __name__ == '__main__':
+    funciones = '''
+        def suma(a, b):
+        def multipli(a: int, b: int) -> int:
+        def error espacios():
+        def imprimir():
+        def sinDosPuntos()
+        def sin_argumentos():
+        defmalformada: '''
+    cant = AnaPython.countDef(funciones)
+    table.add_row(f"{cant} ", "0")
+    console.print(table)
